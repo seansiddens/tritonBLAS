f38a3c51ca76b1128eb702ac7a74ce8808a2f842
Sean Siddens <seansiddens@gmail.com>
2025-09-11 19:42:04 -0500

    Chiplet transform helper

---
+++ include/tritonblas/internal/util.py
@@ -0,0 +1,12 @@
+@triton.jit()
+def chiplet_transform(
+    pid,
+    num_workgroups,
+    num_xcds
+):
+    xcd = pid % num_xcds 
+    pos_in_xcd = pid // num_xcds 
+    min_per_xcd = num_workgroups // num_xcds 
+    extra_sms = num_workgroups % num_xcds 
+    offset = xcd * min_per_xcd + min(xcd, extra_sms)
+    return  = offset + pos_in_xcd
